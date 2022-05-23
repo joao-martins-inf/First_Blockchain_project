9dd31c365a66bd6f58661cc6908b3ce717974390
jmartins <160221033@estudantes.ips.pt>
2022-05-23 16:37:14 +0100

    first contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0; // state of the smart contract on the blockchain
+    uint public taskCount = 0; // state of the variable in the smart contract (inside of the blockchain)
+
+    struct Task {
+        uint id; // id of the task
+        string content; // content/text of the task
+        bool completed; // checkbox state of the todolist
+    }
+
+    mapping(uint => Task) public tasks; //hash with key value pair
+
+    constructor() public {
+        createTask("My first task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
